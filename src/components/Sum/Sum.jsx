58fc7aaf36fa6daca036867da593dcc08c903747
akhvarastyan-web <a.khvarastyan@gmail.com>
2026-01-01 19:47:17 +0200

    third

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -4,6 +4,6 @@ export const Sum = ({ a = 0, b = 0 }) => {
   const result = a + b;
 
   return (
-    <p>Sum of {a} and {b} is {result}</p>
+    <p>{`Sum of ${a} and ${b} is ${result}`}</p>
   );
 };
